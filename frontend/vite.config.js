b2bb849191b8c90fa99ec2fae36d1a8d10bdc56f
Alejandro261102 <alejandrohdz.2626@gmail.com>
2026-05-20 12:20:03 -0600

    cambios

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Toda petición del front que empiece con /api será redirigida al backend
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
